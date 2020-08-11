067ca87a0fa8c480674db97910f1d629d462f1dd
nunorralves <nunorralves@gmail.com>
2020-08-11 23:18:06 +0100

    Add Center decorator to component Button

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Button from "./Button";
+import Center from "../../decorators/Center/Center";
 
 export default {
   title: "Nuno/Button",
   component: Button,
+  decorators: [(story) => <Center>{story()}</Center>],
 };
 
-export const Primary = () => <Button variant='primary'>Primary</Button>
-export const Success = () => <Button variant='success'>Primary</Button>
-export const Danger = () => <Button variant='danger'>Primary</Button>
+export const Primary = () => <Button variant='primary'>Primary</Button>;
+export const Success = () => <Button variant='success'>Primary</Button>;
+export const Danger = () => <Button variant='danger'>Primary</Button>;
