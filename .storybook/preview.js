ca61c521cb38b298de2c86455b852770069fe618
nunorralves <nunorralves@gmail.com>
2020-08-11 23:22:12 +0100

    Add Global Decorator - Center - to preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
+import React from "react";
+import Center from "../src/decorators/Center/Center";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <Center>
+      <Story />
+    </Center>
+  ),
+];
